99c3ea1e48295518f3edb3de05aa31be1a391dc1
Henry <servicemry@sina.com>
2018-03-14 12:36:05 +0800

    netcore

---
+++ netcore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number=500000000;
+            var startTime =DateTime.Now;
+            System.Console.WriteLine("性能测试：循环5亿次所用时间");
+            System.Console.WriteLine("当前开始时间:"+startTime);
+            var count=0;
+            for(int i=1;i<=number;i++)
+            {
+                if(i%2==0){
+                    count+=i;
+                }
+            }
+            System.Console.WriteLine("偶数和"+count);
+           var stopTime=DateTime.Now;
+           System.Console.WriteLine("当前结束时间:"+stopTime);
+           System.Console.WriteLine(stopTime-startTime);
+        }
+    }
+}
